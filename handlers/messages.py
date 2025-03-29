29e36894ec527731171edef666dc71f7ebe17a48
RenamQ <valeriyrenamq@gmail.com>
2025-03-29 16:34:29 +0500

    Reply with uploaded image and filename

--- handlers/messages.py
+++ handlers/messages.py
@@ -1,7 +1,7 @@
-import os
+import io
 
 from aiogram import F, Router, Bot
-from aiogram.types import Message
+from aiogram.types import Message, BufferedInputFile
 from aiogram.fsm.context import FSMContext
 
 from keyboards.inline_keyboard import back_home_kb
@@ -11,16 +11,22 @@ start_msg_router = Router()
 
 @start_msg_router.message(F.photo, UserStates.waiting_for_image)
 async def handle_image(message: Message, state: FSMContext, bot: Bot):
+    state_data = await state.get_data()
+    original_message_id = state_data.get('original_message_id')
 
     image = message.photo[-1]
-    folder = "images"
-    if not os.path.exists(folder):
-        os.makedirs(folder)
     file_info = await bot.get_file(image.file_id)
-    file_name = os.path.join(folder, f"{message.message_id}_{image.file_unique_id}.jpg")
-    await bot.download_file(file_info.file_path, destination=file_name)
+
+    buffer = io.BytesIO()
+    await bot.download_file(file_info.file_path, destination=buffer)
+    buffer.seek(0)
+
+    file_bytes = buffer.getvalue()
+    file_name = f"{message.message_id}_{image.file_unique_id}.jpg"
+    input_file = BufferedInputFile(file=file_bytes, filename=file_name)
 
     await state.clear()
+    await message.delete()
 
     if original_message_id:
         await bot.delete_message(chat_id=message.chat.id, message_id=original_message_id)
