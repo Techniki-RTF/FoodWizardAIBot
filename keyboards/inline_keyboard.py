c481f9f727cc6ad154e98d4018ce4ea76feb9de8
RenamQ <valeriyrenamq@gmail.com>
2025-04-16 19:20:40 +0500

    feat: add profile in main menu - Profile button in Main menu keyboard - Goal and params keyboards for Profile menu

--- keyboards/inline_keyboard.py
+++ keyboards/inline_keyboard.py
@@ -3,14 +3,40 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 def main_menu_kb():
     inline_kb_list = [
         [InlineKeyboardButton(text="Отправить изображение", callback_data='send_image')],
+        [InlineKeyboardButton(text="Профиль", callback_data='profile')],
         [InlineKeyboardButton(text="О боте", callback_data='about')]
     ]
     return InlineKeyboardMarkup(inline_keyboard=inline_kb_list)
 
 
-
 def back_home_kb():
     inline_kb_list = [
+        [InlineKeyboardButton(text="На главную", callback_data='back_home')]
+    ]
+    return InlineKeyboardMarkup(inline_keyboard=inline_kb_list)
+
+
+def profile_kb():
+    inline_kb_list = [
+        [InlineKeyboardButton(text="Изменить цель похудения", callback_data='goal')],
+        [InlineKeyboardButton(text="Изменить личные параметры", callback_data='params')],
+        [InlineKeyboardButton(text="На главную", callback_data='back_home')]
+    ]
+    return InlineKeyboardMarkup(inline_keyboard=inline_kb_list)
+
+def goal_kb():
+    inline_kb_list = [
+        [InlineKeyboardButton(text="Похудение", callback_data='lose_weight')],
+        [InlineKeyboardButton(text="Поддержание веса", callback_data='maintain_weight')],
+        [InlineKeyboardButton(text="Набор массы", callback_data='gain_mass')]
+    ]
+    return InlineKeyboardMarkup(inline_keyboard=inline_kb_list)
+
+def params_kb():
+    inline_kb_list = [
+        [InlineKeyboardButton(text="Изменить рост", callback_data='c_height')],
+        [InlineKeyboardButton(text="Изменить вес", callback_data='c_weight')],
+        [InlineKeyboardButton(text="Изменить возраст", callback_data='c_age')],
         [InlineKeyboardButton(text="На главную", callback_data='back_home')]
     ]
     return InlineKeyboardMarkup(inline_keyboard=inline_kb_list)
