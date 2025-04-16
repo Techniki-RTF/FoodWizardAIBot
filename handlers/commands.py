72751ea847f6fee17289e720b08b48507982d207
RenamQ <valeriyrenamq@gmail.com>
2025-04-16 19:21:44 +0500

    feat: add handlers for profile, goal and body params callback data

--- handlers/commands.py
+++ handlers/commands.py
@@ -2,8 +2,9 @@ from aiogram import Router, F
 from aiogram.filters import CommandStart
 from aiogram.types import Message, CallbackQuery
 from aiogram.fsm.context import FSMContext
-from keyboards.inline_keyboard import main_menu_kb, back_home_kb
 from states import UserStates
+from keyboards.inline_keyboard import *
+from utils.converters import *
 
 start_cmd_router = Router()
 
@@ -32,3 +33,34 @@ async def about(callback: CallbackQuery):
              "Команда: Техники",
              reply_markup=back_home_kb())
 
+@start_cmd_router.callback_query(F.data == 'profile')
+async def profile(callback: CallbackQuery):
+    await callback.message.edit_text(
+        f"Ваша цель: {None}\n"
+        f"Ваши текущие параметры: {None} см / {None} кг / {None} лет\n",
+        reply_markup=profile_kb())
+
+@start_cmd_router.callback_query(F.data == 'goal')
+async def goal(callback: CallbackQuery):
+    await callback.message.edit_text(
+        f"Выберете цель:",
+        reply_markup=goal_kb())
+
+@start_cmd_router.callback_query(F.data == 'params')
+async def params(callback: CallbackQuery):
+    await callback.message.edit_text(
+        f"Выберете параметр для изменения его значения",
+        reply_markup=params_kb())
+
+@start_cmd_router.callback_query(F.data.in_({'lose_weight', 'maintain_weight', 'gain_mass'}))
+async def c_goal(callback: CallbackQuery):
+    await callback.message.edit_text(
+        f"Вы выбрали {goal_converter(callback.data)} в качестве цели",
+        reply_markup=back_home_kb())
+
+@start_cmd_router.callback_query(F.data.in_({'c_height', 'c_weight', 'c_age'}))
+async def c_param(callback: CallbackQuery):
+    await callback.message.edit_text(
+        f"Отправьте значение параметра в формате {params_converter(callback.data)}",
+        reply_markup=back_home_kb()
+    )
