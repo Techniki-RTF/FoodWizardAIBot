6ce6cfdba9c21eb683cdef78a312f85878fe6157
RenamQ <valeriyrenamq@gmail.com>
2025-04-18 15:33:53 +0500

    refactor: add support for main_menu_message_id and error handling

--- handlers/messages.py
+++ handlers/messages.py
@@ -34,19 +34,29 @@ async def handle_image(message: Message, state: FSMContext, bot: Bot):
     file_name = f"{message.message_id}_{image.file_unique_id}.png"
     input_file = BufferedInputFile(file=file_bytes, filename=file_name)
 
+    main_menu_message_id = state_data.get('menu_message_id')
     await state.clear()
+    if main_menu_message_id:
+        await state.update_data(menu_message_id=main_menu_message_id)
+
     await message.delete()
     await delete_original_message(state_data.get('original_message_id'), bot, message)
 
+    answer = await message.answer(text='Распознавание в процессе...')
+    await bot.send_chat_action(message.chat.id, 'upload_photo')
     response = generate_nutrition(file_bytes)
-    if not response:
-        await message.answer(text='Не удалось распознать еду на фото, попробуйте другое изображение', reply_markup=no_response_kb())
-    else:
-        dish_data = [(dish['dish'], dish['weight'], dish['calories_per_100g'], dish['calories_per_total']) for dish in response['dishes']]
-        output = ''
-        for dish_name, dish_weight, dish_calories_per_100g, dish_total_calories in dish_data:
-            output += f"Название блюда: {dish_name}\nВес: {dish_weight}г\nКалории (100г): {dish_calories_per_100g} ккал\nКалории ({dish_weight}г): {dish_total_calories} ккал\n\n"
-        await message.answer_photo(photo=input_file, caption=f'{output}', reply_markup=image_response_kb())
+    match response:
+        case False:
+            await answer.edit_text('Не удалось распознать еду на фото, попробуйте другое изображение', reply_markup=no_response_kb())
+        case 'api_error':
+             await answer.edit_text('Произошла непредвиденная ошибка, попробуйте ещё раз', reply_markup=no_response_kb())
+        case _:
+            dish_data = [(dish['dish'], dish['weight'], dish['calories_per_100g'], dish['calories_per_total']) for dish in response['dishes']]
+            output = ''
+            for dish_name, dish_weight, dish_calories_per_100g, dish_total_calories in dish_data:
+                output += f"Название блюда: {dish_name}\nВес: {dish_weight}г\nКалории (100г): {dish_calories_per_100g} ккал\nКалории ({dish_weight}г): {dish_total_calories} ккал\n\n"
+            await answer.delete()
+            await message.answer_photo(photo=input_file, caption=f'{output}', reply_markup=image_response_kb())
 
 @start_msg_router.message(UserStates.waiting_for_param)
 async def handle_param(message: Message, state: FSMContext, bot: Bot):
