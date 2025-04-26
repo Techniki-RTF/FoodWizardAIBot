2484011f28358fc9e1cbe9b37c63c3b908899560
RenamQ <valeriyrenamq@gmail.com>
2025-04-26 22:44:49 +0500

    fix: remove redundant code

--- utils/gemini.py
+++ utils/gemini.py
@@ -7,13 +7,6 @@ from utils.gemini_constants import *
 client = genai.Client(api_key=config("GEMINI_API_KEY"))
 model = "gemini-2.0-flash-lite"
 
-# async def create_cache(response, image_bytes):
-#     try:
-#         return client.caches.create(model=model, config=types.CreateCachedContentConfig(system_instruction=SYSTEM_INSTRUCTION, contents=[types.Part.from_text(text=SYSTEM_INSTRUCTION), types.Part.from_bytes(data=image_bytes, mime_type="image/png"), types.Part.from_text(text=response)], ttl="300s"))
-#     except APIError as e:
-#         print(e)
-#         return 'api_error'
-
 async def recognize_dish(image_bytes):
     contents = [types.Content(role="user", parts=[types.Part.from_text(text="Первый этап"), types.Part.from_bytes(data=image_bytes, mime_type="image/png")])]
     generate_content_config = types.GenerateContentConfig(response_mime_type="application/json", response_schema=RESPONSE_SCHEMA_FOR_RECOGNITION)
